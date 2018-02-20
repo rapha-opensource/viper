4282c610bccdbf436f4fc61f2207551b9ab1b48b
Raphael Goyran <rapha.open.source@gmail.com>
2018-02-20 11:34:44 -0800

    Change range to be constexpr

--- 17/headers/range.h
+++ 17/headers/range.h
@@ -1,3 +1,6 @@
+#ifndef __VIPER_RANGE__
+#define __VIPER_RANGE__
+
 #include <iterator>
 #include <utility>
 
@@ -9,7 +12,7 @@ class RangeIterator {
     T _value;
 
     public:
-        RangeIterator(const T value):_value(value) {}
+        constexpr RangeIterator(const T value):_value(value) {}
 
         using difference_type = typename std::iterator_traits<T*>::difference_type;
         using value_type = typename std::iterator_traits<T*>::value_type;
@@ -17,28 +20,28 @@ class RangeIterator {
         using reference = typename std::iterator_traits<T*>::reference;
         using iterator_category = typename std::iterator_traits<T*>::iterator_category;
 
-        inline bool operator==(const RangeIterator& rhs) const {
+        constexpr inline bool operator==(const RangeIterator& rhs) const {
             return _value == rhs._value;
         }
 
-        inline bool operator!=(const RangeIterator& rhs) const {
+        constexpr inline bool operator!=(const RangeIterator& rhs) const {
             return _value != rhs._value;
         }
 
-        inline difference_type operator-(const RangeIterator& rhs) const {
+        constexpr inline difference_type operator-(const RangeIterator& rhs) const {
             return _value - rhs._value;
         }
 
-        inline T operator*() const {
+        constexpr inline T operator*() const {
             return _value;
         }
 
-        inline RangeIterator& operator++() {
+        constexpr inline RangeIterator& operator++() {
             ++_value;
             return *this;
         }
 
-        inline auto operator++(int) {
+        constexpr inline auto operator++(int) {
             auto iterator{*this};
             operator++();
             return iterator;
@@ -54,21 +57,18 @@ class Range {
     RangeIterator<T> _start, _stop;
 
     public:
-        Range(const T start, const T stop):_start(start), _stop(stop) {}
+        constexpr Range(const T start, const T stop):_start(start), _stop(stop) {}
 
-        inline RangeIterator<T> begin() const { return _start; }
+        constexpr inline RangeIterator<T> begin() const { return _start; }
 
-        inline RangeIterator<T> end() const { return _stop; }
+        constexpr inline RangeIterator<T> end() const { return _stop; }
 
 };
 
 
 template<class T>
-Range<T> range(const T& start, const T& stop) {
+constexpr Range<T> range(const T& start, const T& stop) {
     return Range<T>(start, stop);
 }
 
-//Iterable<class T, class Distance> range(const T& start, const T& stop, const Distance& step) {
-//}
-
- 
+#endif 
--- 17/tests/range.cpp
+++ 17/tests/range.cpp
@@ -5,16 +5,28 @@
 #include "../headers/iterator_cast.h"
 
 
+constexpr unsigned int sum_1_10() {
+    auto _1_through_10 = range(1,11);
+    unsigned int ret = 0;
+    for( auto i : range(1,11) ) {
+        ret += i;
+    }
+    return ret;
+}
+
+template <unsigned int N> unsigned int sumN() { return N; }
+
+
 TEST_CASE(" 'range' function ", " [range] ") {
 
-    SECTION(" into ", "[into]") {
+    SECTION(" into ") {
         auto vi = into<std::vector<int>>(range(1,5));
 
         REQUIRE( vi == decltype(vi){1,2,3,4} );
     }
 
 
-    SECTION(" range-for ", "[range-for]") {
+    SECTION(" range-for ") {
 
         int k {1};
 
@@ -23,4 +35,9 @@ TEST_CASE(" 'range' function ", " [range] ") {
         }
 
     }
+
+    SECTION(" constexpr range ") {
+        REQUIRE( sumN<sum_1_10()>() == 55 );
+    }
+
 }
